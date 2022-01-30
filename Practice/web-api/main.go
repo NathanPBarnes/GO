bb1955ec7b6092c94a917323b6cebdb0d02fc948
NathanPBarnes <70610511+NathanPBarnes@users.noreply.github.com>
2022-01-29 16:02:23 -0800

    first successful GET request

--- Practice/web-api/main.go
+++ Practice/web-api/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("test")
+	resp, err := http.Get("https://google.com")
+	//We use an If statement to check for errors
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	//We use an If statement to check for errors
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
 }
